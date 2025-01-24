0766f4dcc6ac1c04e13590eb957ed31f4afc1c47
진소희 <130043092+soheeGit@users.noreply.github.com>
2025-01-24 17:55:02 +0900

    index 수정

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 async function makeIssue() {
-    const token = Process.env.GH_TOKEN;
-    const OWNER = "soheeGit";
-    const REPO = "git_power_action";
+    const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
+    // 요고 1
+    const OWNER = "soheeGit"; // 여러분들의 github 계정 이름
+    // 요고 2
+    const REPO = "git_power_action"; // 여러분들의 현재 리포지터리 이름
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title: "행운의 숫자",
-            body: `${Math.floor(Math.random() * 100) + 1}`,
+            title: "행운의 숫자", // 요고 3
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
         })
     });
-    if(response.ok){
+    if (response.ok) {
         console.log("성공");
-    }else{
+    } else {
         console.log("실패");
     }
 }
